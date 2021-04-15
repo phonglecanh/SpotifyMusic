e20bf5308ca104ccff45af43903b733e770696ea
phonglecanh <phonglecanh@gmail.com>
2021-04-15 08:54:43 +0700

    update part 4

--- Spotify/Controllers/Other/ProfileViewController.swift
+++ Spotify/Controllers/Other/ProfileViewController.swift
@@ -4,10 +4,10 @@
 //
 //  Created by Lê Cảnh Phong on 12/04/2021.
 //
-
+import SDWebImage
 import UIKit
-
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
 
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -15,6 +15,8 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
+    
+    private var models = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +41,7 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
                     self?.updateUI(with: model)
                     break
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    print("Profile Error: \(error.localizedDescription)")
                     self?.failedToGetProfile()
                 }
             }
@@ -49,9 +51,31 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     private func updateUI(with model: UserProfile) {
         tableView.isHidden = false
         // config table models
+        models.append("Full name: \(model.display_name)")
+        models.append("Email Address: \(model.email)")
+        models.append("User ID: \(model.id)")
+        models.append("Plan: \(model.product)")
+        createTableHeader(with: model.images.first?.url)
         tableView.reloadData()
     }
 
+    private func createTableHeader(with string: String?) {
+        guard let urlString = string,
+              let url = URL(string: urlString) else {
+            return
+        }
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.width/1.5))
+        let imageSize: CGFloat = headerView.height/2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.contentMode = .scaleAspectFill
+        imageView.sd_setImage(with: url, completed: nil)
+        imageView.layer.masksToBounds = true // bo tron hinh anh
+        imageView.layer.cornerRadius = imageSize/2 // bo tron hinh anh
+        tableView.tableHeaderView = headerView
+    }
+    
     private func failedToGetProfile() {
         let label = UILabel(frame: .zero)
         label.text  = "Failed to load Profile"
@@ -60,17 +84,21 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         view.addSubview(label)
         label.center = view.center
     }
+    
+    
+    // MARK: - Table View
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
 }
 
-// MARK: - Table View
 
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = "Foo"
-    cell.selectionStyle = .none
-    return cell
-}
