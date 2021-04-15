47e75a69157e1d51b9f9b13ffc2613e758a3d68b
phonglecanh <phonglecanh@gmail.com>
2021-04-15 09:43:55 +0700

    part 5: add logo spotify

--- Spotify/Resources/AppDelegate.swift
+++ Spotify/Resources/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
+        
         if AuthManager.shared.isSignedIn {
+            AuthManager.shared.refreshIfNeeded(completion: nil)
             window.rootViewController = TabBarViewController()
         }
         else {
